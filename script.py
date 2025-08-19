385c9327a1a94d6ce8be0ecfd80b57be5513ef6a
enrico <enrico.faraldo@sptech.school>
2025-08-19 18:58:31 -0300

    Corrigindo bugs e permitindo multiplos valores no arquivo csv

--- script.py
+++ script.py
@@ -3,16 +3,17 @@ import psutil
 import pandas
 import datetime
 
+primeiraVez = True
 while True:
     usoCpu = psutil.cpu_percent(10, percpu=False)
     usoRam = psutil.virtual_memory()
     usoDisco = psutil.disk_usage("/")
-    agora = datetime.datetime.now().timestamp()
+    agora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    print(agora)
     print(usoCpu)
     print(usoRam.percent)
     print(usoDisco.percent)
-    print(agora)
 
     dadosCpuRamDisco = {
         "timestamp": agora,
@@ -21,7 +22,15 @@ while True:
         "disco": usoDisco.percent
     }
 
-    dataFrame = pandas.DataFrame(dadosCpuRamDisco)
-    dataFrame.to_csv("dados-servidor.csv", encodings="utf-8", index=False)
 
+    dataFrame = pandas.DataFrame([dadosCpuRamDisco])
+
+    #Estrutura condicional necessaria para evitar renomear o cabeçalho e permitir a inserção de novas linhas com o "mode = a"
+    if primeiraVez:
+        dataFrame.to_csv("dados-servidor.csv", encoding="utf-8", index=False)
+
+    else:
+        dataFrame.to_csv("dados-servidor.csv", encoding="utf-8", mode="a", index=False, header=False)
+    
+    primeiraVez = False
     time.sleep(10)
