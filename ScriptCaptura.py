ec72333c8517a392c05253262d8ce843d010fc1d
enrico <enrico.faraldo@sptech.school>
2025-08-22 13:30:30 -0300

    melhorando o script de captura

--- ScriptCaptura.py
+++ ScriptCaptura.py
@@ -9,13 +9,17 @@ while True:
     uso_ram = psutil.virtual_memory()
     uso_disco = psutil.disk_usage("/")
     agora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    nome_usuario = "Enrico"
 
-    print(agora)
-    print(uso_cpu)
-    print(uso_ram.percent)
-    print(uso_disco.percent)
+    print("nome do usuário: " + nome_usuario)
+    print("agora: " + agora)
+    print("uso de cpu (%): ", uso_cpu)
+    print("uso de ram (%): ", uso_ram.percent)
+    print("uso de disco (%): ", uso_disco.percent)
+    print("\n")
 
     dados_cpu_ram_disco = {
+        "nome de usuario": nome_usuario,
         "timestamp": agora,
         "cpu": uso_cpu,
         "ram": uso_ram.percent,
@@ -27,9 +31,9 @@ while True:
 
     #Estrutura condicional necessaria para evitar renomear o cabeçalho e permitir a inserção de novas linhas com o "mode = a"
     if not os.path.isfile("dados-servidor.csv"):
-        df.to_csv("dados-servidor.csv", encoding="utf-8", index=False)
+        df.to_csv("dados-servidor.csv", encoding="utf-8", index=False, sep=";")
 
     else:
-        df.to_csv("dados-servidor.csv", encoding="utf-8", mode="a", index=False, header=False)
+        df.to_csv("dados-servidor.csv", encoding="utf-8", mode="a", index=False, header=False, sep=";")
     
     time.sleep(10)
